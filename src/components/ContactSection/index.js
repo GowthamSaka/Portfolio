1d979eafff295d1fa9d73bd881ea413d10816d7f
GowthamSaka <gouthamsaka9999@gmail.com>
2021-07-19 10:55:05 +0000

    portfolio

---
+++ src/components/ContactSection/index.js
@@ -0,0 +1,22 @@
+// Write your code here
+import './index.css'
+
+const ContactSection = () => (
+  <div>
+    <h1 className="heading">My Contact</h1>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/location-img.png"
+      className="location-image"
+      alt="location-map"
+    />
+    <p className="contact-description">
+      If you like my projects and intersected to do work with me. Please
+      contact...
+    </p>
+    <p className="contact-information">Please Contact</p>
+    <p className="contact-number">9123456789</p>
+    <p className="email">sophie.millers@gmail.com</p>
+  </div>
+)
+
+export default ContactSection
---
+++ src/components/HomeSection/index.js
@@ -0,0 +1,18 @@
+// Write your code here
+import './index.css'
+
+const HomeSection = () => (
+  <div className="home-section">
+    <h1 className="username">Sophie Millers</h1>
+    <p className="designation">Full Stack Developer</p>
+    <p className="job-description">
+      Experienced in MongoDb, Express, ReactJs and Node.js, Bootstrap, HTML,
+      CSS, SQL and Python.
+    </p>
+    <button className="view-button" type="button">
+      View Resume
+    </button>
+  </div>
+)
+
+export default HomeSection
